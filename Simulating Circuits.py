98054e8dcddb88a3a78750960d9a126a9eaf59d0
Arda Arvas <138934850+Ardarvas@users.noreply.github.com>
2024-03-08 21:47:08 +0300

    Update Simulating Circuits.py

--- Simulating Circuits.py
+++ Simulating Circuits.py
@@ -1,3 +1,5 @@
+#q1
+OPENQASM 2.0;
 include "qelib1.inc";
 
 qreg q[4];
@@ -5,3 +7,37 @@ creg c[4];
 reset q[0];
 x q[0];
 measure q[0] -> c[0];
+
+
+#q2
+OPENQASM 2.0;
+include "qelib1.inc";
+
+qreg q[4];
+creg c[4];
+reset q[0];
+measure q[0] -> c[0];
+x q[0];
+
+#q3
+
+include "qelib1.inc";
+
+qreg q[4];
+creg c[4];
+reset q[1];
+h q[1];
+measure q[1] -> c[1];
+
+#4
+
+
+include "qelib1.inc";
+
+qreg q[4];
+creg c[4];
+reset q[0];
+h q[0];
+h q[0];
+measure q[0] -> c[0];
+
